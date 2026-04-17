c7549a11ad8a1a3c867aabf56ad78a257176909c
marten silbiger <marten@metal-heart.org>
2026-04-16 21:21:23 -0700

    Bermuda braincode to platform-io

---
+++ embedded/Bermuda/octo-identifier.ino
@@ -0,0 +1,245 @@
+/*
+Identify each strand on the octo board by lighting them up in a pattern.
+
+The MIT License (MIT)
+
+Copyright (c) 2018-2023 Marten Silbiger
+https://github.com/dramamine/lightdream-scripts
+
+Copyright (c) 2014 Nathanaël Lécaudé
+https://github.com/natcl/Artnet, http://forum.pjrc.com/threads/24688-Artnet-to-OctoWS2811
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+Resources:
+https://www.pjrc.com/teensy/td_libs_OctoWS2811.html
+
+*/
+
+#include <SPI.h>
+#include <OctoWS2811.h>
+#include <FastLED.h>
+
+// i.e. LEDs per output.
+#define LED_WIDTH 1024
+
+// i.e. how many strips; Octo board supports 8 channels out
+#define LED_HEIGHT 8
+
+// if true, program expects to be plugged into a network switch. If it's not,
+// it will get stuck at `setup()::artnet.begin()`.
+// ## Troubleshooting the network
+// If you see "Link status (should be 2)"
+bool useNetwork = false;
+
+// make sure the config above is correct for your setup. we expect the controlling
+// software  to send (LED_HEIGHT * universesPerStrip) universes to this IP.
+const int ledsPerUniverse = 170;
+
+// Send fps timing to Serial out, should be around 40 fps
+bool showFps = false;
+
+// how long is our update look taking to render?
+// for reference: runs about 12us for regular, 32-universe code
+// LD algorithm Q3-2023 was running 15-17us for 8-universe code
+bool showTiming = false;
+
+// ~~ end config ~~
+
+// how many universes per strip?
+const int universesPerStrip = ceil(LED_WIDTH / 170.0);
+
+const int maxUniverses = LED_HEIGHT * universesPerStrip;
+
+const int numLeds = LED_WIDTH * LED_HEIGHT;
+DMAMEM int displayMemory[LED_WIDTH * 6];
+int drawingMemory[LED_WIDTH * 6];
+const int config = WS2811_GRB | WS2811_800kHz;
+OctoWS2811 leds(LED_WIDTH, displayMemory, drawingMemory, config);
+
+
+// FastLED template
+
+template <EOrder RGB_ORDER = RGB,
+          uint8_t CHIP = WS2811_800kHz>
+class CTeensy4Controller : public CPixelLEDController<RGB_ORDER, 8, 0xFF>
+{
+    OctoWS2811 *pocto;
+
+public:
+    CTeensy4Controller(OctoWS2811 *_pocto)
+        : pocto(_pocto){};
+
+    virtual void init() {}
+    virtual void showPixels(PixelController<RGB_ORDER, 8, 0xFF> &pixels)
+    {
+
+        uint32_t i = 0;
+        while (pixels.has(1))
+        {
+            uint8_t r = pixels.loadAndScale0();
+            uint8_t g = pixels.loadAndScale1();
+            uint8_t b = pixels.loadAndScale2();
+            pocto->setPixel(i++, r, g, b);
+            pixels.stepDithering();
+            pixels.advanceData();
+        }
+
+        pocto->show();
+    }
+};
+
+CRGB rgbarray[numLeds];
+CTeensy4Controller<GRB, WS2811_800kHz> *pcontroller;
+
+namespace Pattern {
+  const int BRIGHTNESS = 127; // out of 255
+
+  void setup() {
+    pcontroller = new CTeensy4Controller<GRB, WS2811_800kHz>(&leds);
+
+    FastLED.setBrightness(BRIGHTNESS);
+    FastLED.addLeds(pcontroller, rgbarray, numLeds);
+    // rgbarray[5] = CHSV(20, 255, 255);
+  }
+
+  void fadeall() { for(int i = 0; i < numLeds; i++) { rgbarray[i].nscale8(250); } }
+
+  void _twinkle() {
+    int i = random16(numLeds);                                           // A random number. Higher number => fewer twinkles. Use random16() for values >255.
+    if (i < numLeds) rgbarray[i] = CHSV(random(255), random(255), random(255));              // Only the lowest probability twinkles will do. You could even randomize the hue/saturation. .
+    for (int j = 0; j < numLeds; j++) rgbarray[j].fadeToBlackBy(8);
+
+    FastLED.show();                                                // Standard FastLED display
+    Serial.println("showing...");
+    //show_at_max_brightness_for_power();                          // Power managed FastLED display
+
+    //delay(10);                                            // Standard delay
+    FastLED.delay(10);                                     // FastLED delay
+    //delay_at_max_brightness_for_power(thisdelay);              // Power managed FastLED delay
+  }
+
+
+  void _cylon() {
+    static uint8_t hue = 0;
+    // First slide the led in one direction
+    for(int i = 0; i < numLeds; i++) {
+        // Set the i'th led to red
+        rgbarray[i] = CHSV(hue++, 255, 255);
+        // Show the leds
+        FastLED.show();
+        // now that we've shown the leds, reset the i'th led to black
+        // leds[i] = CRGB::Black;
+        fadeall();
+        // Wait a little bit before we loop around and do it again
+        delay(10);
+    }
+    Serial.print("x");
+
+    // Now go in the other direction.
+    for(int i = (numLeds)-1; i >= 0; i--) {
+        // Set the i'th led to red
+        rgbarray[i] = CHSV(hue++, 255, 255);
+        // Show the leds
+        FastLED.show();
+        // now that we've shown the leds, reset the i'th led to black
+        // leds[i] = CRGB::Black;
+        fadeall();
+        // Wait a little bit before we loop around and do it again
+        delay(10);
+    }
+  }
+
+  void _every10() {
+    for (int i = 0; i < LED_HEIGHT; i++) {
+      for(int j = 9; j < LED_WIDTH; j+=10) {
+          // Set the i'th led to red
+          rgbarray[j + i*LED_WIDTH] = CHSV(0 + i * 30, 255, 255);
+          FastLED.show();
+          delay(100);
+      }
+    }
+  }
+
+  void _solid() {
+    static uint8_t hue = 0;
+
+    for (int i = 0; i < LED_HEIGHT; i++) {
+      for(int j = 0; j < LED_WIDTH; j++) {
+          // Set the i'th led to red
+          rgbarray[j + i*LED_WIDTH] = CHSV(hue, 255, 255);
+
+      }
+    }
+    hue = hue + 3;
+    FastLED.show();
+    delay(100);
+  }
+
+  void _identifier() {
+    static uint8_t hue = 0;
+
+    for (int i = 0; i < LED_HEIGHT; i++) {
+      for(int j = 0; j < LED_WIDTH; j++) {
+        if ((j % (i + 2)) > 0) {
+          rgbarray[j + i*LED_WIDTH] = CHSV(hue + i*30, 255, 255);
+        } else {
+          rgbarray[j + i*LED_WIDTH] = CRGB::Black;
+        }
+      }
+    }
+    hue = hue + 3;
+    FastLED.show();
+    delay(100);
+  }
+
+  void loop()
+  {
+    // good for stress test
+    // _identifier();
+    // good for counting LEDs
+    // _every10();
+    _solid();
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(2000);
+  Serial.println("INFO:   Filename: octo-identifier");
+  Serial.println("INFO:   Version: 2026.01");
+  Serial.printf("INFO:   LED counter: %d pixels, %d LEDs \n", leds.numPixels(), numLeds);
+  Serial.println();
+
+  // Turn on Teensy onboard LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  leds.begin();
+
+  Pattern::setup();
+}
+
+
+
+void loop()
+{
+  Pattern::loop();
+}
